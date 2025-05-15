f78bc042eb3e6caecd7f527e3e884941294f8df0
Emmanuel Mutombo <emmanuelmutombo99@gmail.com>
2025-05-15 17:44:02 +0100

    deux first

---
+++ header.php
@@ -0,0 +1,44 @@
+<header>
+    <nav>
+        <img src="./images/logonumber.PNG" alt="le logo de la societe">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="index.php" class="liens">
+                        Acceuil
+                    </a>
+                </li>
+                <li class="services-dropdown">
+                    <a href="services.php" class="liens">
+                        Services <i class="fas fa-chevron-down"></i>
+                    </a>
+                    <div class="dropdown-content">
+                        <a href="#">Architecture</a>
+                        <a href="#">Génie Civil</a>
+                        <a href="#">Commerciale</a>
+                        <a href="#">Hydraulique</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="apropos.php" class="liens">
+                        À propos
+                    </a>
+                </li>
+                <li>
+                    <a href="realisations.php" class="liens active">
+                        Réalisations
+                    </a>
+                </li>
+                <li>
+                    <a href="contact.php" class="liens">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="profil-buger">
+            <div class="burger"><i class="fa-solid fa-bars fa-xl"></i></div>
+        </div>
+        <button class="ct">Faites votre devis</button>
+    </nav>
+</header>
